6948535b44eeba3d9beba640558d9ff74f4b497e
zozo404 <enzocuny66@gmail.com>
2023-12-23 19:05:09 +0100

    ➕ | add dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {}
+    },
+    screens: {
+      iphoneSE: "375px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1600px"
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [],
+  content: [
+    "components/**/*.{vue,js}",
+    "layouts/**/*.vue",
+    "pages/**/*.vue",
+    "static/**/*.html",
+    "composables/**/*.{js,ts}",
+    "plugins/**/*.{js,ts}",
+    "App.{js,ts,vue}",
+    "app.{js,ts,vue}"
+  ]
+}
