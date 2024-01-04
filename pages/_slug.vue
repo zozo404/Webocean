9ce5773849db02157c050259cab450c77857d07b
zozo404 <enzocuny66@gmail.com>
2024-01-04 19:47:12 +0100

    @ -> layouts

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -215,7 +215,7 @@ export default {
   // Ajoutez cette méthode pour la redirection vers la page 404
   beforeMount() {
     if (!this.news && !this.kits && !this.pictures) {
-      this.$router.push("@/error");
+      this.$router.push("../layouts/error");
     }
   },
   methods: {
